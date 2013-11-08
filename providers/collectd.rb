d9e0958977867f5937bf30aa250c39aee7a36646
Rodion Vynnychenko <roddiku@gmail.com>
2013-11-08 11:08:06 +0200

    Added repo url attributes

--- providers/collectd.rb
+++ providers/collectd.rb
@@ -1,34 +1,26 @@
 
-def prepare_debian
-	apt_repository 'wormly' do
-      uri          'http://wormly-deb.s3.amazonaws.com'
-      distribution 'production'
-      components   ['main']
-      keyserver    'keyserver.ubuntu.com'
-      key          '5CAB7232'
-    end
-end
-
-def prepare_rhel
-	keyname = "RPM-GPG-KEY-wormly"
-
-	yum_key keyname do
-		url "http://wormly-rpm.s3.amazonaws.com/public.gpg"
-	end
-
-	yum_repository 'wormly' do
-		url 'http://wormly-rpm.s3.amazonaws.com/'
-		repo_name 'wormly'
-		description 'Home for wormly-collectd package'
-		key keyname
-	end
-end
-
 action :install do
 	if platform_family?("debian")
-		prepare_debian
+		apt_repository 'wormly' do
+			uri          new_resource.aptrepo
+			distribution 'production'
+			components   ['main']
+			keyserver	 'keyserver.ubuntu.com'
+			key	         new_resource.keyid
+		end
 	else
-		prepare_rhel
+		keyname = "RPM-GPG-KEY-wormly"
+
+		yum_key keyname do
+			url new_resource.keyurl
+		end
+
+		yum_repository 'wormly' do
+			url new_resource.yumrepo
+			repo_name 'wormly'
+			description 'Home for wormly-collectd package'
+			key keyname
+		end
 	end
 
 	package "wormly-collectd" do
--- resources/collectd.rb
+++ resources/collectd.rb
@@ -3,6 +3,12 @@ actions :install
 
 attribute :apikey, :kind_of => String, :required => true
 
+attribute :yumrepo, :kind_of => String, :default => "https://wormly-rpm.s3.amazonaws.com/"
+attribute :keyurl, :kind_of => String, :default => "https://wormly-rpm.s3.amazonaws.com/public.gpg"
+
+attribute :aptrepo, :kind_of => String, :default => "https://wormly-deb.s3.amazonaws.com/"
+attribute :keyid, :kind_of => String, :default => "5CAB7232"
+
 def initialize(*args)
   super
   @action = :install
