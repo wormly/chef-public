74bbc0a99f6f8ebb3d094e0dc43c22d40952bc81
Rodion Vynnychenko <roddiku@gmail.com>
2013-11-20 15:49:29 +0200

    Made apikey name attribute

--- resources/collectd.rb
+++ resources/collectd.rb
@@ -1,15 +1,11 @@
 
 actions :install
+default_action :install
 
-attribute :apikey, :kind_of => String, :required => true
+attribute :apikey, :kind_of => String, :name_attribute => true
 
 attribute :yumrepo, :kind_of => String, :default => "https://wormly-rpm.s3.amazonaws.com/"
 attribute :keyurl, :kind_of => String, :default => "https://wormly-rpm.s3.amazonaws.com/public.gpg"
 
 attribute :aptrepo, :kind_of => String, :default => "https://wormly-deb.s3.amazonaws.com/"
 attribute :keyid, :kind_of => String, :default => "5CAB7232"
-
-def initialize(*args)
-  super
-  @action = :install
-end
